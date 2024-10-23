c5199f95ca27e8a9d2de684cd3d04164d494039b
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-10-23 16:39:03 +0530

    git commit -m"Commit."

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { React } from 'react';
 import './App.scss';
 
 const App = () =>
-	<div className="App">
+	<div className="text-l font-normal underline text-center mb-80px">
 		Ready to start.
 	</div>;
 
